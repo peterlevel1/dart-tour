d6716f39b1c6c93b1f594705d43ce7dcc7170809
cc <dacong@etransfar.com>
2019-06-20 23:25:05 +0800

    map

---
+++ lib/map.dart
@@ -0,0 +1,34 @@
+void main() {
+  basic();
+}
+
+basic() {
+  // tripple way to build a map
+  var map1 = { 'a': 'b' };
+  assert(map1 is Map);
+
+  var map2 = Map();
+  assert(map2 is Map);
+
+  var map3 = Map<int, String>();
+  map3.addAll({ 1: 'aaa', 2: 'bbb' });
+  assert(map3 is Map);
+  assert(map3[1] == 'aaa');
+}
+
+practicalMethods() {
+  var map1 = {
+    'str': 'hello world',
+    'sth': 'haha'
+  };
+
+  assert(map1.containsKey('str'));
+
+  map1.remove('str');
+  assert(!map1.containsKey('str'));
+
+  assert(map1.keys.toList().length == 1);
+
+  map1.putIfAbsent('sth', () => 'haha');
+  assert(map1.containsKey('sth'));
+}
